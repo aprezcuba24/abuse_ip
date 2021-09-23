dc9b8a7fbd867cdf6151dc28edbe3439f8351f29
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2021-09-22 20:35:10 -0400

    My reports

--- app/bot/handlers/__init__.py
+++ app/bot/handlers/__init__.py
@@ -2,7 +2,9 @@ from .start import StartManager
 from .new_report import NewReport
 from .error import error_handler  # noqa
 from .ip_list import IpListManager
+from .my_ip_list import MyIpListManager
 
 start_manager = StartManager()
 new_report_manager = NewReport()
 list_manager = IpListManager()
+my_list_manager = MyIpListManager()
--- app/bot/handlers/my_ip_list.py
+++ app/bot/handlers/my_ip_list.py
@@ -2,16 +2,18 @@ from app.ipabusers.models.ipabusers import IpAbusers
 from typing import Any
 from app.bot.mixins.paginage_mixin import PaginateMixin
 from app.utils.telegram import trans as _
+from app.utils.telegram import CallbackContext, Update
 
 
-class IpListManager(PaginateMixin):
-    queryset = IpAbusers.objects.order_by("-created_at")
+class MyIpListManager(PaginateMixin):
+    def get_queryset(self, update: Update, context: CallbackContext):
+        return IpAbusers.objects.get_by_user(context.user_model).order_by("id", "-created_at")
 
     def _get_item_command(self, item: Any, **kwargs):
         return f"show_ip {str(item.id)}"
 
     def _get_list_header(self, **kwargs):
-        return _("last_report_header")
+        return _("my_reports_header")
 
     def _callback_subfix(self, **kwargs):
-        return "last_report"
+        return "my_reports"
